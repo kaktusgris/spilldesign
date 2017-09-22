8b8138f38ee16b7483834d43bc9e5cc336df41f6
Marton Skjæveland <martonsk@stud.ntnu.no>
2017-09-22 14:42:10 +0200

    First commit. Stage with ball that can be rotated

---
+++ Assets/Scripts/Drawing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawing : MonoBehaviour {
+
+	float x = 0.0f;
+	float y = 0.0f;
+	GL.crea
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void coordinates () {
+		x = Input.mousePosition.x;
+		y = Input.mousePosition.y;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			coordinates ();
+			Debug.Log (x + ", " + y);
+		}
+	}
+}
